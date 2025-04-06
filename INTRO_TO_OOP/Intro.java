3d929cf0ad34b91dd7f07c9bb46935669baf0e6a
mrlii <leetamparega@gmail.com>
2025-04-06 23:47:55 +0000

    All done with introduction

--- INTRO_TO_OOP/Intro.java
+++ INTRO_TO_OOP/Intro.java
@@ -1,19 +1,34 @@
 public class Main {
-    public static class Book {
-        String name;
-        int pages;
-        // Add the author field as String
-        String  author;
 
-        Book(String name, int pages, String author) {
-            this.name = name;
-            this.pages = pages;
+    public static class Book {
+    	int pages;
+        String author;
+        String title;
+        boolean isRented;
+
+        public Book(String title, String author, int pages) {
+            // Initialize all fields using the arguments as values
+            this.title = title;
             this.author = author;
+            this.pages = pages;
+            // Initialize isRented field as false
+            this.isRented = false;
+        }
+        
+        // Complete method with name `checkIfRented` and without returning value
+        void checkIfRented() {
+            if (isRented) {
+                System.out.println("Book is rented!");
+            } else {
+                System.out.println("Book is available!");
+            }
         }
     }
 
     public static void main(String[] args) {
-        // Create the Moby Dick object with Herman Melville as author
-        Book mobyDick = new Book("Moby-Dick", 635, "Herman Melville");
+        // Create a new instance of a book with the given attributes
+        Book book = new Book("Moby-Dick", "Herman Melville", 635);
+        System.out.println("Book{title='" + book.title + ", author='" + book.author + "', pages=" + book.pages +  "}");
+        book.checkIfRented();
     }
 }
