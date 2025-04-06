0d5c97f4995e49d927b122a54065449abef239ef
mrlii <leetamparega@gmail.com>
2025-04-06 23:29:52 +0000

    Java OOP

--- INTRO_TO_OOP/Intro.java
+++ INTRO_TO_OOP/Intro.java
@@ -1,22 +1,19 @@
 public class Main {
     public static class Book {
+        String name;
         int pages;
-        boolean isRented;
+        // Add the author field as String
+        String  author;
 
-        void checkIfRented() {
-        	// Check if the book is rented
-            if (isRented) { 
-            	// Print the number of pages
-                System.out.println("Book with " + pages + " pages is rented!"); 
-            } else {
-            	// Print the number of pages
-                System.out.println("Book with " + pages + " pages is available!"); 
-            }
+        Book(String name, int pages, String author) {
+            this.name = name;
+            this.pages = pages;
+            this.author = author;
         }
     }
-    
+
     public static void main(String[] args) {
-        Book book = new Book();
-        book.checkIfRented();
+        // Create the Moby Dick object with Herman Melville as author
+        Book mobyDick = new Book("Moby-Dick", 635, "Herman Melville");
     }
 }
